48be3b2f27ddacf5b59195548df3082ef72ce839
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-09-26 14:14:25 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,68 @@
+﻿using CajeroMenuC.Servicios;
+using System.Net.Http.Headers;
+
+namespace CajeroMenuC.Controladores
+{
+    /// <summary>
+    /// Clase principal de la app
+    /// 250923 - jzg
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Metodo de entrada de la app
+        /// 250923 - jzg
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(String[] args)
+        {
+
+            MenuInterfaz menuInterfaz = new MenuImplementacion();
+            menuInterfaz.mostrarMendajeBienvenida();
+
+            bool cerrarMenu = false;
+            int opcionSeleccionada;
+
+            //desde la primera iteracion debe cumplirse la condicion
+            while (!cerrarMenu)
+            {
+                opcionSeleccionada = menuInterfaz.mostrarMenuYSeleccion();
+                Console.WriteLine(opcionSeleccionada);
+                switch ( opcionSeleccionada)
+                {
+                    case 0:
+                        Console.WriteLine("[info] - se ejecuta el caso 0");
+                        cerrarMenu=true;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("[info] - se ejecuta el caso 1");
+                        break;
+                    
+                    case 2:
+                        Console.WriteLine("[info] - se ejecuta el caso 2");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("[info] - se ejecuta el caso 3");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("[info] - se ejecuta el caso 4");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("[info] - se ejecuta el caso 5");
+                        break;
+
+                    default:Console.WriteLine("[info] - la opcion deseada no existe.");
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CajeroMenuC.Servicios
+{
+
+    /// <summary>
+    /// Interfaz con la relacion de metodos e la funcionalidad del menu.
+    /// 250923 - jzg
+    /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Metodo que muestra mensaje de bienvenida al cajero.
+        /// </summary>
+        public void mostrarMendajeBienvenida();
+
+        public int mostrarMenuYSeleccion();
+
+    }
+}
